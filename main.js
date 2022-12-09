8ee1df715300893ecf19b1a5aa1f1dc513678a20
MRIDUL ARORA <aroramridul18@gmail.com>
2022-12-09 14:25:33 +0530

    fix: Changing string to Number
    Happens when I complete a transaction and then start another. For if it was 236 and 699 to start with it will show correct output. But let's say after checking change if I change value to 69 and check again this issues occurs.

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const billAmount = document.querySelector("#billAmount");
-const cashGiven = document.querySelector("#cashGiven");
+const billAmt = document.querySelector("#billAmount");
+const cashG = document.querySelector("#cashGiven");
 const checkNotes = document.querySelector("#checkNotes");
 const errorMsg = document.querySelector("#errorMsg");
 const noOfNotes = document.querySelectorAll(".noOfNotes");
+const result = document.querySelectorAll(".whenAnyExceptionHide");
 
 const availableNotes = [2000,500,100,20,10,5,1];
 
 checkNotes.addEventListener("click", function validate(){
+    const billAmount = Number(billAmt.value);
+    const cashGiven = Number(cashG.value);
     hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+
+    if(billAmount > 0){
+        if(cashGiven > billAmount){
+            const amountToBeReturned = cashGiven - billAmount;
             calculateChange(amountToBeReturned);
-        }else{
+        }else if(cashGiven < billAmount){
             showMessage("Gpay kar de bhai");
+        }else{
+            showMessage("Thanks, for shopping with us!");
         }
     }else{
         showMessage("invalid Bill Amount");
@@ -30,9 +36,13 @@ function calculateChange(amount){
 
 function hideMessage(){
     errorMsg.style.display = "none";
+    result[0].style.display = "block";
+    result[1].style.display = "block";
 }
 
 function showMessage(msg){
     errorMsg.innerText = msg;
     errorMsg.style.display = "block";
+    result[0].style.display = "none";
+    result[1].style.display = "none";
 }
